4467c5743920623f079b0d950b5ed47ec4e18e17
Peter Berg <atticusberg@gmail.com>
2023-03-29 17:40:17 -0400

    yaml for main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import json
 from start_dialogue import start_dialogue
-from tokens import get_token_estimates
+import yaml
 
 
 def run():
-    with open('sample_dialogues.json', 'r') as f:
-        sample_dialogues = json.load(f)
-    initial_prompt_a = sample_dialogues.brain_chemistry.initial_prompt_a
-    initial_prompt_b = sample_dialogues.brain_chemistry.initial_prompt_b
+
+    with open('sample_dialogues.yaml', 'r') as f:
+        sample_dialogues = yaml.load(f, Loader=yaml.FullLoader)
+    initial_prompt_a = sample_dialogues['novel']['initial_prompt_a']
+    initial_prompt_b = sample_dialogues['novel']['initial_prompt_b']
     messages = start_dialogue(initial_prompt_a, initial_prompt_b)
-    print(get_token_estimates(messages))
 
 
 if __name__ == '__main__':
